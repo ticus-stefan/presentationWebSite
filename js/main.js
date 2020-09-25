af63405c36db93bafe0d6f0e6d852a5fdc83c071
ticus-stefan <49829832+ticus-stefan@users.noreply.github.com>
2020-09-25 11:28:42 +0300

    an update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 function initMap() {
-const loc={ lat: -34.397, lng: 150.644 }
+const loc={ lat: -35.093, lng: 150.644 }
 
   map = new google.maps.Map(document.querySelector(".map"), {
     center: loc,
-    zoom: 14
+    zoom: 8
   });
   const marker=new google.maps.Marker({
       position:loc, map:map
   })
 }
+$('#navbar a, .btn').on('click', function(event){
+  if(this.bash !==''){
+    event.preventDefault();
+    const hash=this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top-100
+    },800);
+  }
+});
+
+window.addEventListener('scroll',function(){
+  if(window.scrollY>150){
+    document.querySelector('#navbar').style.opacity=0.9
+  }else{
+    document.querySelector('#navbar').style.opacity=1
+  }
+});
